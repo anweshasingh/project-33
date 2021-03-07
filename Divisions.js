7d09505e8ad8772d9169332124be5676c2625d07
anweshasingh <72261344+anweshasingh@users.noreply.github.com>
2021-03-07 11:04:21 +0530

    Update Divisions.js

--- Divisions.js
+++ Divisions.js
@@ -1,18 +1,17 @@
-class Divisions {
-    constructor(x, y, w, h) {
-        var options = {
-
-            isStatic: true
-        }
-        this.body = Bodies.rectangle(x, y, w, h, options);
-        this.w = w;
-        this.h = h;
-        World.add(world, this.body);
+class Division {
+    constructor(x,y,width,height) {
+      var options = {
+          isStatic: true
+      }
+      this.body = Bodies.rectangle(x,y,width,height,options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
     }
-    display() {
-        var pos = this.body.position;
-        rectMode(CENTER);
-        fill("white");
-        rect(pos.x, pos.y, this.w, this.h);
+    display(){
+      var pos =this.body.position;
+      rectMode(CENTER);
+      fill("pink");
+      rect(pos.x, pos.y, this.width, this.height);
     }
-}
+  };
