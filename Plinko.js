c3845feba2f6d5f3222a2404a0dbe43728c142a5
anweshasingh <72261344+anweshasingh@users.noreply.github.com>
2021-03-07 11:03:24 +0530

    Update Plinko.js

--- Plinko.js
+++ Plinko.js
@@ -1,29 +1,33 @@
-class Plinko {
-    constructor(x, y) {
-        var options = {
-            restitution: 1,
-            friction: 0,
-            isStatic:true
+class Plinko{
+    constructor(x, y, r){
+
+        var options={
+            isStatic : true
         }
-        this.r = 10;
+
+        this.r=r;
+
         this.body = Bodies.circle(x, y, this.r, options);
+        
+        
         
         World.add(world, this.body);
     }
-    display() {
+
+    display(){
 
         var pos = this.body.position;
-        var angle = this.body.angle;
+        var angle = this.body.angle
 
         push();
         translate(pos.x, pos.y);
         rotate(angle);
-        imageMode(CENTER);
-        noStroke();
-        fill("white");
-        ellipseMode(RADIUS);
-        ellipse(0,0,this.r,this.r);
-        pop();
-    }
 
+        fill("lightblue");
+        ellipseMode(RADIUS);
+        circle(0, 0, this.r);
+        pop();
+
+        
+        }
 };
